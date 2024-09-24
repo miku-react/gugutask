211d6170a0699e524b90bc660f8bc4b129ff690e
mocha <2795757650@qq.com>
2024-09-24 13:06:51 +0800

    提交

---
+++ src/main/java/org/fengling/gugutask/GugutaskApplication.java
@@ -0,0 +1,13 @@
+package org.fengling.gugutask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GugutaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GugutaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fengling/gugutask/GugutaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fengling.gugutask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GugutaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
